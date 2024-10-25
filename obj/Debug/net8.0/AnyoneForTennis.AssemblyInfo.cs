83a0aece5c9c829f0a7944173e40512553555b1c
RavinduBB <ravindubasura@gmail.com>
2024-10-25 19:51:34 +0600

    Final Testing
    Check and confirm that the webapp is working perfectly.

--- obj/Debug/net8.0/AnyoneForTennis.AssemblyInfo.cs
+++ obj/Debug/net8.0/AnyoneForTennis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnyoneForTennis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c28ed6920b51eeac6412abd549f513a358699642")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578ee2138619446c6d9f99c1ddf19c0f66b816ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnyoneForTennis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnyoneForTennis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
