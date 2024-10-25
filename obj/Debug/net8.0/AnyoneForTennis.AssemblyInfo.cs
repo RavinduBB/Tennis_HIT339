578ee2138619446c6d9f99c1ddf19c0f66b816ed
Ayush7122 <ayush7a24@gmail.com>
2024-10-25 19:49:20 +0600

    Rechecked Basic App Settings

--- obj/Debug/net8.0/AnyoneForTennis.AssemblyInfo.cs
+++ obj/Debug/net8.0/AnyoneForTennis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnyoneForTennis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4214ece5cfd616461122ed491eb10892fdbd86e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c28ed6920b51eeac6412abd549f513a358699642")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnyoneForTennis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnyoneForTennis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
